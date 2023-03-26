926bff8e5e91c603be60a81f3948403ff462ddde
Ian <ian.vgs@hotmail.com>
2023-03-26 14:38:21 -0300

    .

--- components/List/index.tsx
+++ components/List/index.tsx
@@ -157,7 +157,7 @@ const EventosList = (props: any) => {
             </Heading>
             <Text as="p" fontSize="md" marginTop="2">
               Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
+              industry. Lorem Ipsum has been the industrys standard dummy text
               ever since the 1500s, when an unknown printer took a galley of
               type and scrambled it to make a type specimen book.
             </Text>
@@ -194,7 +194,7 @@ const EventosList = (props: any) => {
             </Heading>
             <Text as="p" fontSize="md" marginTop="2">
               Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
+              industry. Lorem Ipsum has been the industrys standard dummy text
               ever since the 1500s, when an unknown printer took a galley of
               type and scrambled it to make a type specimen book.
             </Text>
